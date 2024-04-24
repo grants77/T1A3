2c90a8c347927737f2b89b475eea74ec30981498
grants77 <g.south@me.com>
2024-04-24 22:46:54 +1000

    working on main to welcome screem, check user for login and menu selection

--- main.py
+++ main.py
@@ -1,54 +1,23 @@
 # System Packages
-import os.path
+
 
 # External Packages
 ## Add Colored ##
 
 # Imported Packages
-## From functions.py ##
+import functions
 
-print("Welcome to the FIFA Laws of the Game Exam\n\nPlease enter your email address below")
+def main():
 
-users_file = "exam_users.csv"
-results_file = "exam_results.csv"
-exam_file = "exam_questions.csv"
+    functions.welcome_screen()
 
-# Check if there is a user file - create a new one if it doesn't exist
-if (not os.path.isfile(users_file)):
-    print("\nCreating a new username file\n") 
-    # create a file and open in write 'w' mode
-    users_file = open(users_file, "w")
-    # enter the headings into the file
-    users_file.write("userid,email,name,accredited\n")
-    # close the file
-    users_file.close()
+    functions.user_check()
 
-# Check if there is a results file - create a new one if it doesn't exist
-if (not os.path.isfile(results_file)):
-    print("\nCreating a results file\n")
-    # create a file and open in write 'w' mode
-    results_file = open(results_file, "w")
-    # enter the headings into the file
-    results_file.write("userid,date,score,result\n")
-    # close the file
-    results_file.close()
+    functions.menu_selection()
 
-# Check if there is a questions file - return an error if there isn't
-if (not os.path.isfile(exam_file)):
-    print("\nError - There are no exam questions.\nPlease contact your facilitator to rectify.\n")
+if __name__ == '__main__':
+    main()
 
+        # functions.user_check()
 
-
-# def user_validate():
-
-
-
-def user_menu():
-    print("Main Menu: \n 1. Take New Exam \n 2. View Previous Scores \n 3. View Leaderboard")
-    
-    user_choice = input("Select Menu Number: ")
-    return user_choice
-
-print(user_menu())
-
-print("This is the end of the exam script")
+    # functions.menu_selection()
